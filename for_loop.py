f7f2167da1c19e1a223c9a07051b21dc959a1d8e
YOGEESH K P <yogeesh793@gmail.com>
2025-02-02 18:52:37 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+li=[1,2,"be",4]
+for i in range(4):
+    if li[i]=="be":
+        li[i]="va"
+    print(li[i])
+
+print("************")
+
+li=[1,2,"besanth",4]
+n=len(li)
+print(n)
+print("**********")
+for i in range(n):
+    if li[i]=="besanth":
+        li[i]="vani"
+        print(li[i])
+    print(li[i])
+    
+
+
+
+
