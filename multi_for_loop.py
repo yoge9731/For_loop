6aa7eff9e3792ed45fc120d8b969148902e5cfda
YOGEESH K P <yogeesh793@gmail.com>
2025-02-02 18:53:08 +0530

    Add files via upload

---
+++ multi_for_loop.py
@@ -0,0 +1,18 @@
+# Multiplication for loop
+n=int(input("Enter the number: "))
+for i in range(1,21):
+    print(n,"*",i,"=",n*i)
+
+n=3
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
+
+n=19
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
+
+n=5
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
+
+
